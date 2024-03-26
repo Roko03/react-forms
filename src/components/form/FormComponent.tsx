0a119262729a3f2a99de1f39a8c828791492e7de
Roko <rokoponjarac2@gmail.com>
2024-03-26 15:59:40 +0100

    make form

--- src/components/form/FormComponent.tsx
+++ src/components/form/FormComponent.tsx
@@ -1,5 +1,24 @@
 import styles from "./FormComponent.module.scss";
 import { useForm } from "react-hook-form";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const orderSchema = z.object({
+  email: z.string().email({
+    message: "Unijeli ste pogrešan e-mail",
+  }),
+  name: z.string().min(1, { message: "Unesite ime" }),
+  country: z.string().min(1, { message: "Unesite državu" }),
+  address: z.string().min(1, { message: "Unesite adresu" }),
+  type: z.enum(["cash", "card"], {
+    errorMap: () => ({ message: "Moraš odabrati način plačanja" }),
+  }),
+  checkbox: z.literal(true, {
+    errorMap: () => ({ message: "Moraš prihvatiti uvjete narudžbe" }),
+  }),
+});
+
+type TOrderSchema = z.infer<typeof orderSchema>;
 
 const FormComponent = () => {
   const {
@@ -7,9 +26,9 @@ const FormComponent = () => {
     handleSubmit,
     formState: { errors, isSubmitting },
     reset,
-  } = useForm<any>();
+  } = useForm<TOrderSchema>({ resolver: zodResolver(orderSchema) });
 
-  const onSubmit = (data: any) => {
+  const onSubmit = (data: TOrderSchema) => {
     reset();
 
     console.log(data);
@@ -24,12 +43,16 @@ const FormComponent = () => {
           {...register("email")}
           placeholder="Email adresa..."
         />
-        {errors.email && <p>{`${errors.email.message}`}</p>}
+        {errors.email && (
+          <p className={styles.form__error}>{`${errors.email.message}`}</p>
+        )}
       </div>
       <div className={styles.form__box}>
         <h3>Adresa</h3>
         <input type="text" {...register("name")} placeholder="Ime" />
-        {errors.name && <p>{`${errors.name.message}`}</p>}
+        {errors.name && (
+          <p className={styles.form__error}>{`${errors.name.message}`}</p>
+        )}
         <select {...register("country")}>
           <option value="croatia">Hrvatska</option>
           <option value="bih">Bosna i Hercegovina</option>
@@ -37,7 +60,9 @@ const FormComponent = () => {
           <option value="slovenia">Slovenija</option>
         </select>
         <input type="text" {...register("address")} placeholder="Adresa" />
-        {errors.address && <p>{`${errors.address.message}`}</p>}
+        {errors.address && (
+          <p className={styles.form__error}>{`${errors.address.message}`}</p>
+        )}
       </div>
       <div className={styles.form__box}>
         <h3>Način plaćanja</h3>
@@ -59,14 +84,18 @@ const FormComponent = () => {
           />
           <label htmlFor={"card"}>Kartica</label>
         </div>
-        {errors.type && <p>{`${errors.type.message}`}</p>}
+        {errors.type && (
+          <p className={styles.form__error}>{`${errors.type.message}`}</p>
+        )}
       </div>
       <div className={styles.form__box}>
         <div className={styles.form__box__label}>
           <input type="checkbox" {...register("checkbox")} id="checkbox" />
           <label htmlFor={"checkbox"}>Prihvaćam uvjete narudžbe</label>
         </div>
-        {errors.checkbox && <p>{`${errors.checkbox.message}`}</p>}
+        {errors.checkbox && (
+          <p className={styles.form__error}>{`${errors.checkbox.message}`}</p>
+        )}
       </div>
       <button
         className={styles.form__button}
